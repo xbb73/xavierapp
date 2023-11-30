ff9db41601b51e627209d7a8f24b9a6401c16501
xbb73 <xbb73@outlook.com>
2023-11-30 11:22:00 -0500

    rework thing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import {NoteNavBar, NoteCollection} from "./ui-components";
+import { NoteNavBar, NoteCollection, UINewNote } from "./ui-components";
+import { Routes, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
   return (
     <div className="App"><header className="App-header">
-        <NoteNavBar/><NoteCollection/>
+      <Routes>
+        <Route exact path = '/' element={<div><NoteNavBar/><NoteCollection/></div>} />
+        {/* <Route exact path = '/new' element={<UINewNote/>}/> */}
+      </Routes>
       </header></div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify } from 'aws-amplify';
+import Amplify from '@aws-amplify/core';
+import "@aws-amplify/ui-react/styles.css";
+import {AmplifyProvider} from "@aws-amplify/ui-react";
 import config from './aws-exports';
+import {BrowserRouter} from 'react-router-dom';
+const container = document.getElementById("root");
+const root = createRoot(container)
+
 Amplify.configure(config);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AmplifyProvider>
+        <App />
+      </AmplifyProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
